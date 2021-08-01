7fbc6044937caa95f4938f586a4388ae9a91c220
Mark Turner <mark.turner-7@postgrad.manchester.ac.uk>
2021-08-01 19:54:38 +0100

    flake8 compliance

--- unique.py
+++ unique.py
@@ -1,13 +1,15 @@
-import random
+from random import randint
+
 
 def main():
     unique = ""
     for i in range(10):
-        if random.randint(0, 1):
-            unique += chr(random.randint(97, 122))
+        if randint(0, 1):
+            unique += chr(randint(97, 122))
         else:
-            unique += chr(random.randint(48, 57))
+            unique += chr(randint(48, 57))
     print(unique)
+
 
 if __name__ == "__main__":
     main()
